44f82eac34e075c0bc53141b886f26505e23225c
way2rach <way2rachita@gmail.com>
2023-06-01 20:15:43 +0300

    add borrow components

--- packages/app/src/views/BorrowView.tsx
+++ packages/app/src/views/BorrowView.tsx
@@ -36,12 +36,12 @@ export const BorrowView: FC = ({}) => {
                 {/*amount control*/}
                 <div className="form-control w-full  ">
                   <label className="label">
-                    <span className="label-text text-base-100">Amount</span>
+                    <span className="label-text text-shrub-blue text-md">Amount</span>
                   </label>
                   <input type="text" placeholder="Enter amount"
-                         className="input input-bordered w-full  bg-white border-solid border border-gray-200 text-lg"/>
+                         className="input input-bordered w-full  bg-white border-solid border border-gray-200 text-lg focus:shadow-shrub-thin focus:border-shrub-green-50"/>
                   <label className="label">
-                    <span className="label-text-alt text-gray-500">Wallet Balance:  {wallet &&
+                    <span className="label-text-alt text-gray-500 text-sm font-light">Wallet Balance:  {wallet &&
 
                         <span>
                           {(balance || 0).toLocaleString()} SOL
@@ -55,7 +55,7 @@ export const BorrowView: FC = ({}) => {
                 {/*interest rate control*/}
                 <div className="form-control w-full  ">
                   <label className="label">
-                    <span className="label-text text-base-100">Interest Rate</span>
+                    <span className="label-text text-shrub-blue">Interest Rate</span>
                   </label>
                   <div>
 
@@ -105,7 +105,39 @@ export const BorrowView: FC = ({}) => {
 
                 </div>
 
-                <div className="divider text-base-100 h-0.5 w-full bg-gray-300"></div>
+                <div className="divider h-0.5 w-full bg-gray-100 my-8"></div>
+                {/*spinner */}
+
+                {/*<div className="hero-content flex-col mb-3">*/}
+                {/*  <div role="status">*/}
+                {/*    <svg aria-hidden="true"*/}
+                {/*         className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-shrub-green-500"*/}
+                {/*         viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+                {/*      <path*/}
+                {/*        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"*/}
+                {/*        fill="currentColor"/>*/}
+                {/*      <path*/}
+                {/*        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"*/}
+                {/*        fill="currentFill"/>*/}
+                {/*    </svg>*/}
+                {/*    <span className="sr-only">Loading...</span>*/}
+                {/*  </div>*/}
+                {/*<p className="text-shrub-grey pt-3 text-sm">Calculating required collateral</p>*/}
+                {/*</div>*/}
+
+                {/*display estimate apy*/}
+
+                <div className="hero-content mb-2 flex-col gap-2">
+                  <div className="flex flex-row text-lg ">
+                  <span className="w-[360px]">Required collateral</span>
+                    <span><img src="/sol-logo.svg" className="w-4 inline align-baseline"/> SOL</span>
+                  </div>
+                  <div className="card w-full bg-teal-50 py-10">
+                      <span className="text-5xl text-shrub-green-500 font-bold text-center">0.0123 SOL</span>
+                  </div>
+                </div>
+
+                {/*cta*/}
                 <button className="btn btn-block bg-shrub-green border-0 hover:bg-shrub-green normal-case text-xl">Continue</button>
               </div>
             </div>
