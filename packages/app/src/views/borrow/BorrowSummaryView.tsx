f0f0afc0955d83a5de7c9df6ee2ed62da26a2714
way2rach <way2rachita@gmail.com>
2023-06-30 00:26:04 +0100

    add borrow success state

--- packages/app/src/views/borrow/BorrowSummaryView.tsx
+++ packages/app/src/views/borrow/BorrowSummaryView.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect} from "react";
+import {FC, useEffect, useState} from "react";
 // import {useConnection, useWallet} from "@solana/wallet-adapter-react";
 import useUserSOLBalanceStore from "../../stores/useUserSOLBalanceStore";
 import {useAddress, useContract, useContractWrite, Web3Button} from "@thirdweb-dev/react";
@@ -29,6 +29,8 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
         router.push('/dashboard');
     };
 
+    const [borrowSuccess, setBorrowSuccess] = useState(false);
+
 
     const walletAddress = useAddress();
     const {
@@ -52,13 +54,10 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
     //   }
     // }, [wallet.publicKey, connection, getUserSOLBalance])
 
-    // const numberOfMonths = duration;
 
     // Calculate the end date by adding the number of months to the current date
     const currentDate = new Date();
-    const dateShadow = new Date();
     const endDate = fromEthDate(timestamp);
-    // const endDate = new Date(dateShadow.setMonth(dateShadow.getMonth() + Number(numberOfMonths)));
 
     async function handleTakeLoan() {
 
@@ -82,123 +81,41 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
                         Summary
                     </h1>
                 </div>
-
-                {/*BORROW SUMMARY*/}
-                {/*<div className="relative group mt-4 w-full">*/}
-                {/*    <div className="absolute -inset-1 shadow-shrub border rounded-3xl "></div>*/}
-                {/*    <div className="flex flex-col ">*/}
-                {/*        <div className="card w-full text-left">*/}
-                {/*            <div className="card-body text-base-100">*/}
-                {/*                <p className="text-lg font-bold pb-2">*/}
-                {/*                    Borrow*/}
-                {/*                </p>*/}
-                {/*                <div className="w-full text-xl font-semibold flex flex-row">*/}
-                {/*                    <span className="text-4xl  font-medium text-left w-[500px]">{amount} USDC</span>*/}
-                {/*                    <img src="/usdc-logo.svg" className="w-10 inline align-baseline"/>*/}
-                {/*                </div>*/}
-                {/*                <p className="text-shrub-grey-700 text-lg text-left font-light pt-8 max-w-[550px]">You*/}
-                {/*                    are borrowing <strong>{amount} USDC</strong> and*/}
-                {/*                    giving <strong>{requiredCollateral} ETH</strong> as collateral. There is no*/}
-                {/*                    interest, and you have one month to repay*/}
-                {/*                    the loan. The collateral will be locked until the loan is fully paid, and then it*/}
-                {/*                    will be returned to*/}
-                {/*                    you.</p>*/}
-
-                {/*                <div className="divider h-0.5 w-full bg-gray-100 my-8"></div>*/}
-                {/*                /!*receipt start*!/*/}
-                {/*                <div className="mb-2 flex flex-col gap-3 text-shrub-grey-200 text-lg font-light">*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Required collateral</span>*/}
-                {/*                        <span>{requiredCollateral} ETH</span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Start Date</span>*/}
-                {/*                        <span>{currentDate.toDateString()}</span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Due Date</span>*/}
-                {/*                        <span>{endDate.toDateString()}<img src="/edit.svg"*/}
-                {/*                                                           className="w-5 inline align-baseline ml-2"/></span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Interest Rate ✨</span>*/}
-                {/*                        <span className="font-semibold text-shrub-green-500"> {interestRate}%</span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Wallet</span>*/}
-                {/*                        <span>0x5464e8...36200<img src="/copy.svg"*/}
-                {/*                                                   className="w-6 hidden md:inline align-baseline ml-2"/> </span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row  justify-between">*/}
-                {/*                        <span className="">Contract Address</span>*/}
-                {/*                        <span>0x78s44e8...32sd0<img src="/copy.svg"*/}
-                {/*                                                    className="w-6 hidden md:inline align-baseline ml-2"/> </span>*/}
-                {/*                    </div>*/}
-                {/*                    /!*<div className="flex flex-row  justify-between">*!/*/}
-                {/*                    /!*  <span className="">Late Penalty<img src="/info-circle.svg"*!/*/}
-                {/*                    /!*                                               className="w-6 ml-2 inline"/></span>*!/*/}
-                {/*                    /!*  <span>1%</span>*!/*/}
-                {/*                    /!*</div>*!/*/}
-                {/*                </div>*/}
-
-                {/*                <div className="divider h-0.5 w-full bg-gray-100 my-8"></div>*/}
-
-                {/*                /!*total*!/*/}
-                {/*                <div className="flex flex-col gap-3 mb-6 text-shrub-grey-200 text-lg font-light">*/}
-                {/*                    <div className="flex flex-row justify-between ">*/}
-                {/*                        <span className="">Due today</span>*/}
-                {/*                        <span>{requiredCollateral} ETH</span>*/}
-                {/*                    </div>*/}
-                {/*                    <div className="flex flex-row justify-between">*/}
-                {/*                        <span className="">Gas Cost</span>*/}
-                {/*                        <span>0.0012 ETH</span>*/}
-                {/*                    </div>*/}
-                {/*                </div>*/}
-                {/*                /!*cta*!/*/}
-                {/*                /!*<button*!/*/}
-                {/*                /!*    className="btn btn-block bg-shrub-green border-0 hover:bg-shrub-green-500 normal-case text-xl mb-4"*!/*/}
-                {/*                /!*    onClick={handleTakeLoan}>Deposit*!/*/}
-                {/*                /!*</button>*!/*/}
-                {/*                <Web3Button contractAddress={lendingPlatformAddress} className="!btn !btn-block !bg-shrub-green !border-0 !normal-case !text-xl hover:!bg-shrub-green-500 !mb-4"*/}
-
-                {/*                    // uint256 _amount, // Amount of USDC with 6 decimal places*/}
-                {/*                    // uint256 _collateral, // Amount of ETH collateral with 18 decimal places*/}
-                {/*                    // uint256 _ltv,*/}
-                {/*                    // uint256 _timestamp*/}
-                {/*                            action={() => mutateAsyncTakeLoan({ args: [*/}
-                {/*                                    ethers.utils.parseUnits(amount, 6),*/}
-                {/*                                    ethers.utils.parseEther(requiredCollateral),*/}
-                {/*                                    interestToLTV[interestRate],*/}
-                {/*                                    toEthDate(new Date("2023-08-01"))*/}
-                {/*                                ], overrides: {*/}
-                {/*                                    value: ethers.utils.parseEther(requiredCollateral)*/}
-                {/*                                } })}*/}
-                {/*                >*/}
-                {/*                    Borrow*/}
-                {/*                </Web3Button>*/}
-                {/*                <button onClick={onCancel}*/}
-                {/*                    className="btn btn-block bg-white border text-shrub-grey-700 hover:bg-gray-100 hover:border-shrub-grey-50 normal-case text-xl border-shrub-grey-50">Cancel*/}
-                {/*                </button>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
-    {/*BORROW SUCCESS*/}
                 <div className="relative group mt-4 w-full">
                     <div className="absolute -inset-1 shadow-shrub border rounded-3xl "></div>
                     <div className="flex flex-col ">
-                        <div className="card w-full text-center">
+                        <div className="card w-full">
                             <div className="card-body text-base-100">
-
-
-                                <svg className="w-[400px] h-[400px] text-shrub-green dark:text-white" aria-hidden="true"
+                                {!borrowSuccess && <div>
+                                <p className="text-lg font-bold pb-2 text-left">
+                                    Borrow
+                                </p>
+                                <div className="w-full text-xl font-semibold flex flex-row">
+                                    <span className="text-4xl  font-medium text-left w-[500px]">{amount} USDC</span>
+                                    <img src="/usdc-logo.svg" className="w-10 inline align-baseline"/>
+                                </div>
+                                <p className="text-shrub-grey-700 text-lg text-left font-light pt-8 max-w-[550px]">You
+                                    are borrowing <strong>{amount} USDC</strong> and
+                                    giving <strong>{requiredCollateral} ETH</strong> as collateral. There is no
+                                    interest, and you have one month to repay
+                                    the loan. The collateral will be locked until the loan is fully paid, and then it
+                                    will be returned to
+                                    you.</p>
+                                </div>}
+                                {borrowSuccess &&
+                                  <>
+                                  <p className="text-lg font-bold pb-2 text-left">
+                                      Borrow successful
+                                  </p>
+                                  <svg className="w-[400px] h-[400px] text-shrub-green dark:text-white" aria-hidden="true"
                                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="1" d="m7 10 2 2 4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                          strokeWidth="1" d="m7 10 2 2 4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                                 </svg>
+                                  </>}
 
                                 <div className="divider h-0.5 w-full bg-gray-100 my-8"></div>
+                                {!borrowSuccess && <div>
                                 {/*receipt start*/}
                                 <div className="mb-2 flex flex-col gap-3 text-shrub-grey-200 text-lg font-light">
                                     <div className="flex flex-row  justify-between">
@@ -211,8 +128,9 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
                                     </div>
                                     <div className="flex flex-row  justify-between">
                                         <span className="">Due Date</span>
-                                        <span>{endDate.toDateString()}<img src="/edit.svg"
-                                                                           className="w-5 inline align-baseline ml-2"/></span>
+                                        <span>{endDate.toDateString()}
+                                            <img src="/edit.svg" className="w-5 inline align-baseline ml-2"/>
+                                        </span>
                                     </div>
                                     <div className="flex flex-row  justify-between">
                                         <span className="">Interest Rate ✨</span>
@@ -235,7 +153,7 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
                                     {/*</div>*/}
                                 </div>
 
-
+                                <div className="divider h-0.5 w-full bg-gray-100 my-8"></div>
                                 {/*total*/}
                                 <div className="flex flex-col gap-3 mb-6 text-shrub-grey-200 text-lg font-light">
                                     <div className="flex flex-row justify-between ">
@@ -266,12 +184,18 @@ export const BorrowSummaryView: FC<BorrowSummaryViewProps> = ({onBack, onCancel,
                                                 ], overrides: {
                                                     value: ethers.utils.parseEther(requiredCollateral)
                                                 } })}
+                                            onSuccess={(result) => setBorrowSuccess(true)}
                                 >
                                     Borrow
                                 </Web3Button>
-                                <button onClick={handleViewDash}
+</div>}
+                                {borrowSuccess && <button onClick={handleViewDash}
                                         className="btn btn-block bg-white border text-shrub-grey-700 hover:bg-gray-100 hover:border-shrub-grey-50 normal-case text-xl border-shrub-grey-50">View in Dashboard
-                                </button>
+                                </button>}
+
+                                {!borrowSuccess && <button onClick={onCancel}
+                                        className="btn btn-block bg-white border text-shrub-grey-700 hover:bg-gray-100 hover:border-shrub-grey-50 normal-case text-xl border-shrub-grey-50">Cancel
+                                </button>}
                             </div>
                         </div>
                     </div>
