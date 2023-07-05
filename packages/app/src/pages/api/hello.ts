f4fe5ffd50d960e80e1566ff6353d04df6f2dc71
John Guthrie <jguthrie7@gmail.com>
2023-07-05 15:03:37 +0100

    Remove unused breaking import

--- packages/app/src/pages/api/hello.ts
+++ packages/app/src/pages/api/hello.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { LendingPlatform, PoolShareToken__factory, USDCoin } from '../../../../hardhat/typechain-types';
 
 type Data = {
   name: string
