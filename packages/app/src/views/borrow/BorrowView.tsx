3339bd882ede1cfbe342e818ff8231dbfcc2eea8
way2rach <way2rachita@gmail.com>
2024-02-22 13:48:35 -0500

    fix alert styles

--- packages/app/src/views/borrow/BorrowView.tsx
+++ packages/app/src/views/borrow/BorrowView.tsx
@@ -167,8 +167,11 @@ export const BorrowView: React.FC<BorrowViewProps> = ({ onBorrowViewChange }) =>
         <div className='mt-6 self-start'>
 
           {localError && (
-            <div className="alert alert-warning justify-start">
-              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6 ml-4p" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+            <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 flex items-center" role="alert">
+              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
+              </svg>
               <span>{localError}</span>
             </div>
           )}
