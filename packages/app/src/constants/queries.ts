e548bd0ba15039353a6f80be8be1bf42e948a2bc
way2rach <way2rachita@gmail.com>
2024-03-14 13:39:56 -0400

    extend store logic to lend positions

--- packages/app/src/constants/queries.ts
+++ packages/app/src/constants/queries.ts
@@ -48,142 +48,16 @@ export const USER_POSITIONS_QUERY = gql`
     }
 `;
 
-export const SEED_ADOPTION_QUERY = gql`
-    query SeedAdoption($numResults: Int) {
-        adoptionRecords(
-            first: $numResults
-            orderBy: timestamp
-            orderDirection: desc
-        ) {
-            seed {
-                name
-                type
-            }
-            user {
-                id
-            }
+export const ACTIVE_LENDINGPOOLS_QUERY = gql`
+    query GetActiveLendingPools {
+        lendingPools(where:{finalized:false}){
+            id
             timestamp
+            totalPrincipal
+            totalEthYield
+            totalUsdcInterest
+            tokenSupply
         }
     }
 `;
 
-export const REGISTERED_SIBLINGS_QUERY = gql`
-    query RegisteredSiblings($dnas: [Int], $registered: [String]) {
-        seeds(where: { dna_in: $dnas, owner_in: $registered }) {
-            id
-            dna
-        }
-    }
-`;
-
-export const NFT_LEADERBOARD_QUERY = gql`
-    query NFTLeaderboard($numResults: Int, $b: [String]) {
-        users(
-            first: $numResults
-            orderBy: seedCount
-            orderDirection: desc
-            where: { id_not_in: $b }
-        ) {
-            id
-            seedCount
-            seeds {
-                type
-            }
-        }
-    }
-`;
-
-export const MY_GARDENS_QUERY = gql`
-    query MyGardens($user: String) {
-        _meta {
-            block {
-                number
-            }
-        }
-        user(id: $user) {
-            ticketCount
-            waterCount
-            fertilizerCount
-            potCount
-            pottedPlants {
-                id
-                growth
-                lastWatering
-                lastClaim
-                seed {
-                    name
-                    dna
-                    emotion
-                    type
-                }
-            }
-            shrubNfts {
-                id
-                name
-                uri
-                pottedPlant {
-                    id
-                    seed {
-                        type
-                        emotion
-                        dna
-                    }
-                }
-            }
-        }
-        seeds(where: { owner: $user }) {
-            id
-            name
-            dna
-            emotion
-            type
-        }
-    }
-`;
-
-export const SEED_OWNERSHIP_QUERY = gql`
-    query SeedOwnership($address: String) {
-        user(id: $address) {
-            seeds {
-                id
-                type
-                name
-                emotion
-                dna
-                born
-                bornBlock
-            }
-        }
-    }
-`;
-
-export const GARDENS_STATS_QUERY = gql`
-    query Stats {
-        typeStats {
-            virgin
-            unmoved
-            treasury
-            planted
-            id
-            harvested
-            claimed
-            circulation
-            burned
-        }
-        pottedPlants(
-            orderBy: growth
-            orderDirection: desc
-            where: { growth_gt: 0, growth_lt: 10000 }
-        ) {
-            id
-            growth
-            owner {
-                id
-            }
-            uri
-        }
-        users(where: { potCount_gt: "0" }) {
-            potCount
-        }
-    }
-`;
