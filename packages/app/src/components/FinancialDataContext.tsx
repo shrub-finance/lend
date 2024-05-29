1a271c2b915b605dd276f6004cafb8f0591fb89d
John Guthrie <jguthrie7@gmail.com>
2024-05-29 17:07:39 -0400

    Fix Deposit Summary so that button prompts to connect if not connected

--- packages/app/src/components/FinancialDataContext.tsx
+++ packages/app/src/components/FinancialDataContext.tsx
@@ -146,7 +146,6 @@ export const FinancialDataProvider: React.FC<{children: ReactNode}> = ({ childre
   }, [activeLendingPoolsData]);
 
   useEffect(() => {
-    console.log("running walletAddress useEffect");
     if (!walletAddress) {
       return;
     }
@@ -179,6 +178,12 @@ export const FinancialDataProvider: React.FC<{children: ReactNode}> = ({ childre
     }
   }, [userPositionsData]);
 
+  useEffect(() => {
+    if (!userPositionsDataError) {
+      return;
+    }
+    console.error(userPositionsDataError)
+  }, [userPositionsDataError]);
 
   return (
     <FinancialDataContext.Provider value={{ store, dispatch }}>
