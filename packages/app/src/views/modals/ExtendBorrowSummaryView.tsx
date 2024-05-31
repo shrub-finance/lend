a0cc71e5941eb83920881d23326086465d0b2df6
way2rach <way2rachita@gmail.com>
2024-05-31 18:31:08 -0400

    remove bolding from modal descriptions

--- packages/app/src/views/modals/ExtendBorrowSummaryView.tsx
+++ packages/app/src/views/modals/ExtendBorrowSummaryView.tsx
@@ -5,7 +5,7 @@ import {
   calcPercentage,
   ethInUsdc,
   formatLargeUsdc,
-  formatPercentage, formatWad,
+  formatPercentage, formatShortDate, formatWad,
   fromEthDate, interestToLTV, ltvToInterest,
   toEthDate,
 } from '../../utils/ethMethods';
@@ -126,11 +126,8 @@ const ExtendBorrowSummaryView: React.FC<ExtendBorrowSummaryProps & {
                      height='40'/>
             </div>
             <p className='text-shrub-grey-700 text-lg text-left font-light pt-8 max-w-[550px]'>
-              When you extend this borrow of
-              <span className='font-bold'>{' '}{formatLargeUsdc(borrow.debt)} USDC</span>, your
-              existing borrow position
-              token will be burnt and a new one will be minted reflecting the new date <span
-              className='font-bold'>{newEndDate ? fromEthDate(newEndDate).toLocaleString() : toEthDate(store.platformData.activePoolTimestamps[store.platformData.activePoolTimestamps.length - 1])}</span>.
+              When you extend this borrow of {formatLargeUsdc(borrow.debt)} USDC, your
+              existing borrow position token will be burnt and a new one will be minted reflecting the new date {newEndDate ? fromEthDate(newEndDate).toLocaleString() : toEthDate(store.platformData.activePoolTimestamps[store.platformData.activePoolTimestamps.length - 1])}.
             </p>
             <div className='divider h-0.5 w-full bg-shrub-grey-light3 my-8'></div>
             {/*receipt start*/}
@@ -145,22 +142,22 @@ const ExtendBorrowSummaryView: React.FC<ExtendBorrowSummaryProps & {
                   <span>{formatLargeUsdc(borrow.principal)} USDC</span>
                 </div>
                 <div className='flex flex-row  justify-between'>
-                  <span className=''>Old Due Date</span>
-                  <span>{borrow.endDate.toLocaleString()}
-                  </span>
-                </div>
-                <div className='flex flex-row  justify-between'>
-                  <span className=''>Current LTV</span>
-                  <span>
-                    {formatPercentage(ltv)}%
+                  <span className=''>Current Due Date</span>
+                  <span>{borrow.endDate.toDateString()}
                   </span>
                 </div>
                 <div className='flex flex-row  justify-between cursor-pointer'
                      onClick={(e) => onBackExtend('dateChangeRequest')}>
                   <span>New Due Date ✨</span>
                   <span className='font-semibold text-shrub-green-500'>
-                    {newEndDate ? fromEthDate(newEndDate).toLocaleString() : toEthDate(store.platformData.activePoolTimestamps[store.platformData.activePoolTimestamps.length - 1])}<Image
+                    {newEndDate ? fromEthDate(newEndDate).toDateString() : toEthDate(store.platformData.activePoolTimestamps[store.platformData.activePoolTimestamps.length - 1])}<Image
                     alt='edit icon' src='/edit.svg' className='w-5 inline align-baseline ml-2' width='20' height='20' /></span>
+                </div>
+                <div className='flex flex-row  justify-between'>
+                  <span className=''>Current LTV</span>
+                  <span>
+                    {formatPercentage(ltv)}%
+                  </span>
                 </div>
                 <div className='flex flex-row  justify-between cursor-pointer'
                      onClick={(e) => onBackExtend('ltvChangeRequest')}>
