2c58a568d5879795316f2292fbf1a5e95320f2d4
way2rach <way2rachita@gmail.com>
2024-08-05 13:52:31 -0700

    update modal titles

--- packages/app/src/views/modals/extend-borrow/ExtendBorrowView.tsx
+++ packages/app/src/views/modals/extend-borrow/ExtendBorrowView.tsx
@@ -75,7 +75,7 @@ setIsModalOpen, borrow
   return (
     <>
       <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
-        <h3 className="text-xl font-semibold text-shrub-grey-900 ">Extend</h3>
+        <h3 className="text-xl font-semibold text-shrub-grey-900 ">Extend Borrow</h3>
         <button type="button" onClick={closeModalAndPassData} className="text-shrub-grey-400 bg-transparent hover:bg-shrub-grey-100 hover:text-shrub-grey-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center  ">
           <svg className="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
