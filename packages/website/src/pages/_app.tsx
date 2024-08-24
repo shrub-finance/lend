1fee0cc4b23910bffad2e5c5668381654a06caaf
way2rach <way2rachita@gmail.com>
2024-08-24 15:35:06 -0700

    cleanup

--- packages/website/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// pages/_app.tsx
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
