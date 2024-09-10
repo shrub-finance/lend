a45278ff2586484b70909db4c523e71318bec758
Queen Bud <way2rachita@gmail.com>
2024-09-09 18:03:31 -0700

    add home page (#151)

--- packages/app/src/pages/home.tsx
+++ packages/app/src/pages/home.tsx
@@ -1,50 +1,71 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import {useState} from "react";
-import {useRouter} from "next/router";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = (props) => {
-
-  const [requiredCollateral, setRequiredCollateral] = useState<string | undefined>(undefined);
+  const [requiredCollateral, setRequiredCollateral] = useState<
+    string | undefined
+  >(undefined);
   const [duration, setDuration] = useState<string | undefined>(undefined);
-  const [interestRate, setInterestRate] = useState<string | undefined>(undefined);
+  const [interestRate, setInterestRate] = useState<string | undefined>(
+    undefined,
+  );
   const [amount, setAmount] = useState<string | undefined>(undefined);
   const router = useRouter();
 
   const handleBorrow = () => {
-    router.push('/borrow');
+    router.push("/borrow");
   };
   return (
     <>
       <Head>
         <title>Shrub Lend - Home</title>
-        <meta
-          name="description"
-          content="Lend"
-        />
+        <meta name="description" content="Lend" />
       </Head>
 
-      <div className="md:hero mx-auto m-40">
+      <div className="md:hero mx-auto">
         <div className="md:hero-content flex flex-col">
-          <div className='self-start'>
+          <div className="self-start">
             <div className="w-full">
               <div className="flex flex-col ">
-                <div className="card w-full text-center">
-                  <div className="card-body">
-                    <div className="text-center">
-                      {/*heading*/}
-                      <p className=" text-5xl w-100 leading-relaxed font-semibold">
-                        Borrow or lend USDC
+                <div className="h-screen flex justify-center items-center">
+                  <div className="card w-full text-center">
+                    <div className="card-body flex flex-col items-center">
+                      <p className="font-bold text-[36px] md:text-[48px] lg:text-[72px] leading-tight md:leading-[56px] lg:leading-[90px] tracking-tight md:tracking-[-0.72px] lg:tracking-[-1.44px] bg-gradient-to-r from-[#1B8E8E] to-[#38F6C9] bg-clip-text text-transparent">
+                        0% interest loans
                       </p>
-                      <p className=" text-5xl w-100 font-semibold ">
-                        with <span className="text-shrub-green font-medium">fixed</span> rates
+                      <p className="w-full max-w-[506px] text-[16px] md:text-[18px] lg:text-[20px] font-normal leading-[24px] md:leading-[28px] lg:leading-[30px] text-[#475467] mt-6">
+                        Borrow USDC against your ETH with full flexibility.
+                        Leave your loan open or pay it back whenever you want
+                        with for as low as 0-8% APR.{" "}
                       </p>
-
-                      <p className=" text-5xl w-100 leading-relaxed font-light mb-10">
-                        (0 -8% APR)
-                      </p>
-
-                      <button className="btn btn-block bg-shrub-green border-0 hover:bg-shrub-green-500 normal-case text-xl" onClick={handleBorrow}>Get Started</button>
+                      <div className="mt-12">
+                        <button
+                          className="flex items-center justify-center w-[213px] px-7 py-4 gap-5 rounded-[33px] border border-[#101828] bg-[#101828] font-semibold text-white shadow-[0px_1px_2px_rgba(16,24,40,0.05)] hover:bg-[hsl(221,43%,11%)]"
+                          onClick={handleBorrow}
+                        >
+                          Launch App
+                          <svg
+                            width="24"
+                            height="25"
+                            viewBox="0 0 24 25"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g id="arrow-right">
+                              <path
+                                id="Icon"
+                                d="M5 12.5H19M19 12.5L12 5.5M19 12.5L12 19.5"
+                                stroke="white"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </g>
+                          </svg>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -53,8 +74,6 @@ const Home: NextPage = (props) => {
           </div>
         </div>
       </div>
-
-
     </>
   );
 };
--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { DashboardView } from "../views/DashboardView";
 import Borrow from "./borrow";
+import Home from "./home";
 
 // we are using dashboard view also as the home view, can be changed in the future
 const Dashboard: NextPage = () => {
@@ -11,6 +12,7 @@ const Dashboard: NextPage = () => {
         <title>Shrub Lend - Dashboard</title>
         <meta name="description" content="Dashboard" />
       </Head>
+      {/*<Home />*/}
       {/*<DashboardView/>*/}
       <Borrow />
     </div>
