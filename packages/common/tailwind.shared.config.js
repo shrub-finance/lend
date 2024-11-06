fa3c30cad9a190062c67e5436342c53bb47689ba
Rachita Guthrie <way2rachita@gmail.com>
2024-11-06 14:47:37 -0800

    Color adjustments (#158)
    * color consistent with brand
    
    * add framer

--- packages/common/tailwind.shared.config.js
+++ packages/common/tailwind.shared.config.js
@@ -14,7 +14,6 @@ const sharedConfig = {
         "shrub-green-300": "#219f80",
         "shrub-green-400": "#73e2a3",
         "shrub-green-500": "#17725C",
-        "shrub-green-700": "#115D49",
         "shrub-green-900": "#0A4736",
         "shrub-blue": "#101828",
         "shrub-grey": "#475467",
