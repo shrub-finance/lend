e039025706269ab49ae5d89c559ec48ac75f5ec5
oddwes <oddwes3.0@gmail.com>
2024-11-12 18:51:02 -0800

    lowers header

--- packages/app/src/views/dashboard/Card.tsx
+++ packages/app/src/views/dashboard/Card.tsx
@@ -1,6 +1,6 @@
 export const Card = ({children}) => {
   return (
-    <div className="relative overflow-x-auto group mt-4 w-full rounded-2xl border h-max">
+    <div className="relative bg-white overflow-x-auto group mt-4 w-full rounded-2xl border border-slate-200 h-max">
       {children}
     </div>
   )
